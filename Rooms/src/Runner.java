fbada7e7eb875d481118ab62c233273cb0ea4aea
pjdan <pjdan@DESKTOP-5CQOPLM.home>
2018-01-07 22:45:12 -0500

    new changes

--- Rooms/src/Runner.java
+++ Rooms/src/Runner.java
@@ -96,7 +96,8 @@ public class Runner
 		
 		//Create a board.
 		Board game = new Board(building);
-
+		
+		game.printMap();
 		//Setup player 1 and the input scanner
 		Person player1 = new Person("FirstName", "FamilyName", 0,0);
 		building[0][0].enterRoom(player1);
